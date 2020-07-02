22a9736afd9ca69b4d008d7d299decce4ff4e606
ntholi <ntholi@live.com>
2020-07-02 15:20:35 +0200

    Add check for null in AuditRevisionListener.newRevision

--- src/main/java/com/breakoutms/lfs/server/audit/AuditRevisionListener.java
+++ src/main/java/com/breakoutms/lfs/server/audit/AuditRevisionListener.java
@@ -6,12 +6,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.val;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class AuditRevisionListener implements RevisionListener {
 
 	@Override
 	public void newRevision(Object revisionEntity) {
 		AuditRevisionInfo rev = (AuditRevisionInfo) revisionEntity;
+		if(isAuthenticationObjectNull()) {
+			return;
+		}
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		if (principal instanceof UserDetails) {
 			val userId = ((UserDetails)principal).getUsername();
@@ -19,6 +24,22 @@ public class AuditRevisionListener implements RevisionListener {
 				rev.setUserId(Integer.valueOf(userId));
 			}
 		}
+	}
+
+	private boolean isAuthenticationObjectNull() {
+		if(SecurityContextHolder.getContext() == null) {
+			log.error("Unable to add Audit info because SecurityContextHolder.getContext() is null");
+			return true;
+		}
+		if(SecurityContextHolder.getContext().getAuthentication() == null) {
+			log.error("Unable to add Audit info because SecurityContextHolder.getContext().getAuthentication() is null");
+			return true;
+		}
+		if(SecurityContextHolder.getContext().getAuthentication().getPrincipal() == null) {
+			log.error("Unable to add Audit info because SecurityContextHolder.getContext().getAuthentication().getPrincipal() is null");
+			return true;
+		}
+		return false;
 	}
 
 }
